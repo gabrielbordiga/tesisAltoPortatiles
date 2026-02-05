45c6a8fe6ce163ff7fe0fda35a4117e351c4daf4
Valentina Catala <valentinacatala6@gmail.com>
2026-02-04 23:42:02 -0300

    Stock de unidades dinamico

--- back/routes/unidadRoutes.js
+++ back/routes/unidadRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const unidadController = require('../controllers/unidadController');
 
 router.get('/tipos', unidadController.getTipos);
-router.post('/tipos', unidadController.crearTipo);
 router.get('/resumen', unidadController.getResumenStock);
+router.get('/disponibilidad', unidadController.getDisponibilidadPorRango);
+router.post('/tipos', unidadController.crearTipo);
 router.post('/gestion', unidadController.gestionarStock);
 
 module.exports = router;
